95b748e0173b252c8c1e925e48b7aa68bd35561f
FilipCepiga <filipcepiga@student.agh.edu.pl>
2023-12-05 15:03:51 +0100

    Dodanie plików zrodlowych

--- src/storage_types.cpp
+++ src/storage_types.cpp
@@ -1,3 +1,16 @@
 //
 // Created by Filip Cepiga on 28/11/2023.
 //
+#include "storage_types.hpp"
+
+Package PackageQueue::pop() {
+    Package package;
+    if (queue_type_ == PackageQueueType::FIFO) {
+        package = std::move(queue_.front());
+        queue_.pop_front();
+    } else if (queue_type_ == PackageQueueType::LIFO) {
+        package = std::move(queue_.back());
+        queue_.pop_back();
+    }
+    return package;
+}
