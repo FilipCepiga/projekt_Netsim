7e6410adbd95aff72a4927276d548eeeef961040
FilipCepiga <filipcepiga@student.agh.edu.pl>
2023-12-12 16:21:03 +0100

    Dodanie plikow zrodlowych do 15.12

--- src/helpers.cpp
+++ src/helpers.cpp
@@ -1,3 +1,14 @@
 //
 // Created by Filip Cepiga on 12/12/2023.
 //
+
+#include <random>
+
+std::random_device rd;
+std::mt19937 rng(rd());
+
+double default_probability_generator() {
+    return std::generate_canonical<double, 10>(rng);
+}
+
+std::function<double()> probability_generator = default_probability_generator;
